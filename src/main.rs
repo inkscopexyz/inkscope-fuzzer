f35372a66ca19449483b4e04806bf9d5ea177577
0xLucca <0xlucca.dev@gmail.com>
2023-12-20 17:33:24 -0300

    Some configs from pallet-contracts

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::error::Error;
 use wasmi::*;
+use wasmi::{Config as WasmiConfig, Linker, Memory, MemoryType, StackLimits, Store};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // First step is to create the Wasm execution engine with some config.
     // In this example we are using the default configuration.
-    let engine = Engine::default();
     let wat = r#"
         (module
             (import "host" "hello" (func $host_hello (param i32)))
@@ -15,30 +15,63 @@ fn main() -> Result<(), Box<dyn Error>> {
     "#;
     // Wasmi does not yet support parsing `.wat` so we have to convert
     // out `.wat` into `.wasm` before we compile and validate it.
-    let wasm = wat::parse_str(&wat)?;
-    let module = Module::new(&engine, &mut &wasm[..])?;
+    let wasm = wat::parse_str(wat)?;
 
-    // All Wasm objects operate within the context of a `Store`.
-    // Each `Store` has a type parameter to store host-specific data,
-    // which in this case we are using `42` for.
+    let determinism = false; // We assume that the code is not using floating point determinism
+    let stack_limits = StackLimits::default();
+    let memory_limits = (0, 200); //TODO: set this to a reasonable value
     type HostState = u32;
-    let mut store = Store::new(&engine, 42);
+    let host_state: HostState = 42;
+    let consume_fuel = false; //TODO:This should be set to true to match pallet_contracts
+
+    let mut config = WasmiConfig::default();
+    config
+        .wasm_multi_value(false)
+        .wasm_mutable_global(false)
+        .wasm_sign_extension(true)
+        .wasm_bulk_memory(false)
+        .wasm_reference_types(false)
+        .wasm_tail_call(false)
+        .wasm_extended_const(false)
+        .wasm_saturating_float_to_int(false)
+        .floats(determinism)
+        .consume_fuel(consume_fuel)
+        .fuel_consumption_mode(FuelConsumptionMode::Eager)
+        .set_stack_limits(stack_limits);
+
+    let engine = Engine::new(&config);
+    let module =
+        Module::new(&engine, &wasm[..]).map_err(|_| "Can't load the module into wasmi!")?;
+
+    let mut store = Store::new(&engine, host_state);
+    let mut linker: Linker<HostState> = Linker::new(&engine);
+
+    // Here we allocate this memory in the _store_. It allocates _inital_ value, but allows it
+    // to grow up to maximum number of memory pages, if necessary.
+
+    let memory = Memory::new(
+        &mut store,
+        MemoryType::new(memory_limits.0, Some(memory_limits.1)).expect(""),
+    )
+    .expect("");
+
+    linker
+        .define("env", "memory", memory)
+        .expect("We just created the Linker. It has no definitions with this name; qed");
+
     let host_hello = Func::wrap(&mut store, |caller: Caller<'_, HostState>, param: i32| {
         println!("Got {param} from WebAssembly");
         println!("My host state is: {}", caller.data());
     });
 
-    // In order to create Wasm module instances and link their imports
-    // and exports we require a `Linker`.
-    let mut linker = <Linker<HostState>>::new(&engine);
-    // Instantiation of a Wasm module requires defining its imports and then
-    // afterwards we can fetch exports by name, as well as asserting the
-    // type signature of the function with `get_typed_func`.
-    //
-    // Also before using an instance created this way we need to start it.
     linker.define("host", "hello", host_hello)?;
-    let instance = linker.instantiate(&mut store, &module)?.start(&mut store)?;
-    let hello = instance.get_typed_func::<(), ()>(&store, "hello")?;
+
+    let instance = linker
+        .instantiate(&mut store, &module)
+        .map_err(|_| "can't instantiate module with provided definitions")?;
+    // let instance = linker.instantiate(&mut store, &module)?.start(&mut store)?;
+    let started_instance = instance.start(&mut store)?;
+    let hello = started_instance.get_typed_func::<(), ()>(&store, "hello")?;
 
     // And finally we can call the wasm!
     hello.call(&mut store, ())?;
