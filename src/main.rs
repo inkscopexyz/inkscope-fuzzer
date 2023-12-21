792797d2c32700ab2c0fb58cd328c459b53ad6d3
Felipe Manzano <felipe.andres.manzano@gmail.com>
2023-12-21 20:08:32 -0300

    feat: Minimal command line parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,77 @@
 use std::error::Error;
 use wasmi::*;
+use clap::Parser;
+use std::path::PathBuf;
+extern crate wabt;
+
+use wabt::wasm2wat;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The wat file of the contract to fuzz test
+    #[arg(long)]
+    wat: Option<PathBuf>,
+
+    /// The .contract file of the contract to fuzz test
+    #[arg(long)]
+    contract: Option<PathBuf>,
+
+    /// The .wasm file of the contract to fuzz test
+    #[arg(long)]
+    wasm: Option<PathBuf>,
+
+}
+
+
+fn get_wat_from_wat(path: PathBuf) -> Result<String, Box<dyn Error>> {
+    let wat = std::fs::read_to_string(path)?;
+    Ok(wat)
+}
+
+fn get_wat_from_wasm(path: PathBuf) -> Result<String, Box<dyn Error>> {
+    let wasm = std::fs::read(path)?;
+    let wat = wasm2wat(wasm)?;
+    Ok(wat)
+}
+
+fn get_wat_from_contract(path: PathBuf) -> Result<String, Box<dyn Error>> {
+    let contract = std::fs::read_to_string(path)?;
+    let contract: serde_json::Value = serde_json::from_str(&contract)?;
+    let wat = contract["wasm"]["wat"].as_str().unwrap();
+    Ok(wat.to_string())
+}
+
+
+fn get_wat(args: Args) -> Result<String, Box<dyn Error>> {
+    let args_count = args.wat.is_some() as usize + args.contract.is_some() as usize + args.wasm.is_some() as usize;
+    if args_count != 1 {
+        return Err("Please specify exactly one of --wat, --contract, or --wasm".into());
+    }
+    let wat = match args.wat {
+        Some(path) => get_wat_from_wat(path)?,
+        None => match args.contract {
+            Some(path) => get_wat_from_contract(path)?,
+            None => match args.wasm {
+                Some(path) => get_wat_from_wasm(path)?,
+                None => panic!("Please specify exactly one of --wat, --contract, or --wasm"),
+            }
+        }
+    };
+    Ok(wat)
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
     // First step is to create the Wasm execution engine with some config.
     // In this example we are using the default configuration.
     let engine = Engine::default();
-    let wat = r#"
-        (module
-            (import "host" "hello" (func $host_hello (param i32)))
-            (func (export "hello")
-                (call $host_hello (i32.const 3))
-            )
-        )
-    "#;
+    let wat = get_wat(args)?;
+
     // Wasmi does not yet support parsing `.wat` so we have to convert
     // out `.wat` into `.wasm` before we compile and validate it.
-    let wasm = wat::parse_str(&wat)?;
+    let wasm = wat::parse_str(wat)?;
     let module = Module::new(&engine, &mut &wasm[..])?;
 
     // All Wasm objects operate within the context of a `Store`.
@@ -43,4 +99,29 @@ fn main() -> Result<(), Box<dyn Error>> {
     // And finally we can call the wasm!
     hello.call(&mut store, ())?;
     Ok(())
+}
+
+
+
+#[cfg(test)]
+mod tests {
+
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+#[test]
+fn wat_wasm_contract() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("runtime-fuzzer")?;
+
+    cmd.arg("--wat").arg("test/file/doesnt/exist");
+    cmd.arg("--wasm").arg("test/file/doesnt/exist");
+    cmd.arg("--contract").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("Please specify exactly one of --wat, --contract, or --wasm"));
+
+    Ok(())
+}
+
 }
