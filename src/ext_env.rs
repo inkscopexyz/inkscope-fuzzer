edaa71695bbf7af281272983439ced53b360d653
0xLucca <95830307+0xLucca@users.noreply.github.com>
2024-01-14 21:51:49 -0300

    Config wasmi and hostfunctions (#3)
    Co-authored-by: Felipe Manzano <felipe.andres.manzano@gmail.com>

---
+++ src/ext_env.rs
@@ -0,0 +1,164 @@
+use wasmi::*;
+use parity_scale_codec::{Encode, Decode, MaxEncodedLen, DecodeLimit};
+use wasmi::core::Trap;
+use std::collections::HashMap;
+
+pub struct LoadedModule {
+    pub module: Module,
+    pub engine: Engine,
+}
+
+impl LoadedModule {
+    /// Creates a new instance of `LoadedModule`.
+    ///
+    /// The inner Wasm module is checked not to have restricted WebAssembly proposals.
+    /// Returns `Err` if the `code` cannot be deserialized or if it contains an invalid module.
+    pub fn new(
+        code: &[u8],
+        determinism: bool,
+        stack_limits: Option<StackLimits>,
+    ) -> Result<Self, &'static str> {
+        // NOTE: wasmi does not support unstable WebAssembly features. The module is implicitly
+        // checked for not having those ones when creating `wasmi::Module` below.
+        let mut config = Config::default();
+        config
+            .wasm_multi_value(false)
+            .wasm_mutable_global(false)
+            .wasm_sign_extension(true)
+            .wasm_bulk_memory(false)
+            .wasm_reference_types(false)
+            .wasm_tail_call(false)
+            .wasm_extended_const(false)
+            .wasm_saturating_float_to_int(false)
+            .floats(!determinism)
+            .consume_fuel(false)
+            .fuel_consumption_mode(FuelConsumptionMode::Eager);
+
+        if let Some(stack_limits) = stack_limits {
+            config.set_stack_limits(stack_limits);
+        }
+
+        let engine = Engine::new(&config);
+        let module = Module::new(&engine, code).map_err(|_| "Can't load the module into wasmi!")?;
+
+        // Return a `LoadedModule` instance with
+        // __valid__ module.
+        Ok(LoadedModule { module, engine })
+    }
+}
+
+#[derive(Debug)]
+pub struct HostState {
+    pub storage: HashMap<Vec<u8>, Vec<u8>>,
+    pub input_buffer: Vec<u8>,
+    pub caller: [u8; 32],
+    pub value_transferred: u128,
+    pub memory: Option<Memory>,
+    pub return_data: Option<Vec<u8>>,
+}
+/// The maximum nesting depth a contract can use when encoding types.
+const MAX_DECODE_NESTING: u32 = 256;
+
+impl HostState{
+
+    pub fn get_input(&self) -> &[u8] {
+        &self.input_buffer
+    }
+
+    /// Reads and decodes a type with a size fixed at compile time from contract memory.
+    pub fn decode_from_memory<D: Decode + MaxEncodedLen + std::fmt::Debug>(
+        &self,
+        memory: &mut [u8],
+        ptr: u32,
+    ) -> Result<D, Trap> {
+        let ptr = ptr as usize;
+        let mut bound_checked = memory
+            .get(ptr..ptr + D::max_encoded_len() as usize).ok_or(Trap::new(format!("Pointer out of bound reading at {}", ptr)))?;
+
+        D::decode_with_depth_limit(MAX_DECODE_NESTING, &mut bound_checked)
+            .map_err(|_| Trap::new(format!("Error decoding at {}", ptr)))
+
+    }
+
+    pub fn encode_to_memory<E: Encode + MaxEncodedLen + std::fmt::Debug>(   &self,
+        memory: &mut [u8],
+        ptr: u32,
+        value: E,
+    ) -> Result<(), Trap> {
+        let buffer = value.encode();
+        self.write_to_memory(memory, ptr, &buffer)
+    }
+
+
+    pub fn encode_to_memory_bounded<E: Encode + MaxEncodedLen + std::fmt::Debug>(   &self,
+        memory: &mut [u8],
+        ptr: u32,
+        ptr_len: u32,
+        value: E,
+    ) -> Result<(), Trap> {
+        let len =self.decode_from_memory::<u32>(memory, ptr_len)?;
+        let buffer = value.encode();
+        let buffer_len = buffer.len();
+        if buffer_len > len as usize {
+            return Err(Trap::new(format!("Buffer too large to encode at {} with size {}", ptr, len)));
+        }
+        self.write_to_memory(memory, ptr, &buffer)?;
+        self.write_to_memory(memory, ptr_len, 
+            &(buffer_len as u32).encode())
+    }
+
+
+    /// Write the given buffer to the designated location in the memory.
+	pub fn write_to_memory(
+		&self,
+		memory: &mut [u8],
+		ptr: u32,
+		buf: &[u8],
+	) -> Result<(), Trap> {
+		let ptr = ptr as usize;
+		let bound_checked =
+			memory.get_mut(ptr..ptr + buf.len()).ok_or(
+                Trap::new(format!("Pointer out of bound writing at {}", ptr)))?;
+
+
+            bound_checked.copy_from_slice(buf);
+		Ok(())
+	}
+
+    /// read from the designated location in the memory.
+    pub fn read_from_memory<'a>(
+        &self,
+        memory: &'a[u8],
+        ptr: u32,
+        len: u32,
+    ) -> Result<&'a [u8], Trap> {
+		let ptr = ptr as usize;
+
+        let mut bound_checked = memory
+            .get(ptr..ptr + len as usize).ok_or(Trap::new(format!("Pointer out of bound reading at {}", ptr)))?;
+
+        Ok(bound_checked)
+    }
+
+
+    pub fn set_storage(
+        &mut self,
+        memory: &[u8],
+        key_ptr: u32,
+        key_len: u32,
+        value_ptr: u32,
+        value_len: u32,
+    ) -> Result<u32, Trap> {
+        let key = self.read_from_memory(memory, key_ptr, key_len)?;
+        let value = self.read_from_memory(memory, value_ptr, value_len)?;
+        self.storage.insert(key.into(), value.into());
+
+
+        Ok(0)
+    }
+    
+    pub fn set_return_data(&mut self, return_data: &[u8]){
+        self.return_data = Some(return_data.into());
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+mod ext_env;
+mod flipper;
 use clap::Parser;
+use ext_env::*;
+use std::collections::HashMap;
+use std::convert::TryFrom;
 use std::error::Error;
 use std::path::PathBuf;
+use wasmi::core::Trap;
 use wasmi::*;
 extern crate wabt;
 
@@ -66,42 +72,224 @@ fn get_wat(args: Args) -> Result<String, Box<dyn Error>> {
     Ok(wat)
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+/// Stores the input passed by the caller into the supplied buffer.
+///
+/// The value is stored to linear memory at the address pointed to by `out_ptr`.
+/// `out_len_ptr` must point to a u32 value that describes the available space at
+/// `out_ptr`. This call overwrites it with the size of the value. If the available
+/// space at `out_ptr` is less than the size of the value a trap is triggered.
+///
+
+fn host_input_fn(
+    mut ctx: Caller<'_, HostState>,
+    buf_ptr: u32,
+    buf_len_ptr: u32,
+) -> Result<(), Trap> {
+    //TODO: this needs to be a true logging facility
+    println!(
+        "HOSTFN:: input(buf_ptr: 0x{:x}, buf_len_ptr: 0x{:x})",
+        buf_ptr, buf_len_ptr
+    );
+    let (memory, state) = ctx
+        .data()
+        .memory
+        .expect("No memory")
+        .data_and_store_mut(&mut ctx);
+
+    state.decode_from_memory::<u32>(memory, buf_len_ptr)?;
+
+    // TODO generate approiate inpud using host state and seed and abi and whatever
+    let input = state.get_input();
+    let input_len = u32::try_from(input.len()).expect("Buffer length must be less than 4Gigs");
+
+    state.write_to_memory(memory, buf_ptr, input)?;
+    state.encode_to_memory(memory, buf_len_ptr, input_len)
+}
+
+/// Set the value at the given key in the contract storage.
+///
+/// The key and value lengths must not exceed the maximums defined by the contracts module
+/// parameters. Specifying a `value_len` of zero will store an empty value.
+///
+/// # Parameters
+///
+/// - `key_ptr`: pointer into the linear memory where the location to store the value is placed.
+/// - `key_len`: the length of the key in bytes.
+/// - `value_ptr`: pointer into the linear memory where the value to set is placed.
+/// - `value_len`: the length of the value in bytes.
+///
+/// # Return Value
+///
+/// Returns the size of the pre-existing value at the specified key if any. Otherwise
+/// `SENTINEL` is returned as a sentinel value.
+fn host_set_storage(
+    mut ctx: Caller<'_, HostState>,
+    key_ptr: u32,
+    key_len: u32,
+    value_ptr: u32,
+    value_len: u32,
+) -> Result<u32, Trap> {
+    //TODO: this needs to be a true logging facility
+    println!("HOSTFN:: set_storage(key_ptr: 0x{:x}, key_len: 0x{:x}, value_ptr: 0x{:x}, value_len: 0x{:x})", key_ptr, key_len, value_ptr, value_len);
+    let (memory, state) = ctx
+        .data()
+        .memory
+        .expect("No memory")
+        .data_and_store_mut(&mut ctx);
+
+    state.set_storage(memory, key_ptr, key_len, value_ptr, value_len)
+}
+
+/// Cease contract execution and save a data buffer as a result of the execution.
+///
+/// This function never returns as it stops execution of the caller.
+/// This is the only way to return a data buffer to the caller. Returning from
+/// execution without calling this function is equivalent to calling:
+/// ```nocompile
+/// seal_return(0, 0, 0);
+/// ```
+///
+/// The flags argument is a bitfield that can be used to signal special return
+/// conditions to the supervisor:
+/// --- lsb ---
+/// bit 0      : REVERT - Revert all storage changes made by the caller.
+/// bit [1, 31]: Reserved for future use.
+/// --- msb ---
+///
+/// Using a reserved bit triggers a trap.
+fn host_seal_return(
+    mut ctx: Caller<'_, HostState>,
+    flags: i32,
+    data_ptr: u32,
+    data_len: u32,
+) -> Result<(), Trap> {
+    println!(
+        "HOSTFN:: seal_return(flags: 0x{:x}, data_ptr: 0x{:x}, data_len: 0x{:x})",
+        flags, data_ptr, data_len
+    );
+    let (memory, state) = ctx
+        .data()
+        .memory
+        .expect("No memory")
+        .data_and_store_mut(&mut ctx);
+    let return_data = state.read_from_memory(memory, data_ptr, data_len)?;
+    state.set_return_data(return_data);
+    Err(Trap::i32_exit(flags))
+}
+
+/// Stores the value transferred along with this call/instantiate into the supplied buffer.
+///
+/// The value is stored to linear memory at the address pointed to by `out_ptr`.
+/// `out_len_ptr` must point to a `u32` value that describes the available space at
+/// `out_ptr`. This call overwrites it with the size of the value. If the available
+/// space at `out_ptr` is less than the size of the value a trap is triggered.
+///
+/// The data is encoded as `T::Balance`.
+fn value_transferred(
+    mut ctx: Caller<'_, HostState>,
+    out_ptr: u32,
+    out_len_ptr: u32,
+) -> Result<(), Trap> {
+    println!(
+        "HOSTFN:: value_transferred(out_ptr: 0x{:x}, out_len_ptr: 0x{:x})",
+        out_ptr, out_len_ptr
+    );
+    let (mut memory, state) = ctx
+        .data()
+        .memory
+        .expect("No memory")
+        .data_and_store_mut(&mut ctx);
+
+    state.encode_to_memory_bounded(memory, out_ptr, out_len_ptr, state.value_transferred)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    // First step is to create the Wasm execution engine with some config.
-    // In this example we are using the default configuration.
-    let engine = Engine::default();
     let wat = get_wat(args)?;
 
     // Wasmi does not yet support parsing `.wat` so we have to convert
     // out `.wat` into `.wasm` before we compile and validate it.
     let wasm = wat::parse_str(wat)?;
-    let module = Module::new(&engine, &mut &wasm[..])?;
+    let determinism = true;
+    let contract = LoadedModule::new(&wasm, determinism, None).unwrap();
 
-    // All Wasm objects operate within the context of a `Store`.
-    // Each `Store` has a type parameter to store host-specific data,
-    // which in this case we are using `42` for.
-    type HostState = u32;
-    let mut store = Store::new(&engine, 42);
-    let host_hello = Func::wrap(&mut store, |caller: Caller<'_, HostState>, param: i32| {
-        println!("Got {param} from WebAssembly");
-        println!("My host state is: {}", caller.data());
-    });
+    let mut host_state = HostState {
+        storage: HashMap::new(),
+        input_buffer: vec![0xed, 0x4b, 0x9d, 0x1b],
+        caller: [0; 32],
+        value_transferred: 0,
+        memory: None,
+        return_data: None,
+    };
+    let mut store = Store::new(&contract.engine, host_state);
+    let mut linker = Linker::new(&contract.engine);
+    let memory = Memory::new(&mut store, MemoryType::new(2, Some(16)).expect("")).expect("");
+    store.data_mut().memory = Some(memory);
 
-    // In order to create Wasm module instances and link their imports
-    // and exports we require a `Linker`.
-    let mut linker = <Linker<HostState>>::new(&engine);
-    // Instantiation of a Wasm module requires defining its imports and then
-    // afterwards we can fetch exports by name, as well as asserting the
-    // type signature of the function with `get_typed_func`.
-    //
-    // Also before using an instance created this way we need to start it.
-    linker.define("host", "hello", host_hello)?;
-    let instance = linker.instantiate(&mut store, &module)?.start(&mut store)?;
-    let hello = instance.get_typed_func::<(), ()>(&store, "hello")?;
+    let host_get_storage = Func::wrap(
+        &mut store,
+        |caller: Caller<'_, HostState>, param: i32, param1: i32, param2: i32, param3: i32| -> i32 {
+            println!("Hello from host_get_storage");
+            println!("param: {}", param);
+            println!("param1: {}", param1);
+            println!("param2: {}", param2);
+            println!("param3: {}", param3);
+            1
+        },
+    );
+    linker
+        .define("seal1", "get_storage", host_get_storage)
+        .unwrap();
+
+    let host_set_storage = Func::wrap(&mut store, host_set_storage);
+    linker
+        .define("seal2", "set_storage", host_set_storage)
+        .unwrap();
+
+    let host_value_transferred = Func::wrap(&mut store, value_transferred);
+    linker
+        .define("seal0", "value_transferred", host_value_transferred)
+        .unwrap();
+
+    let host_input = Func::wrap(&mut store, host_input_fn);
+
+    linker.define("seal0", "input", host_input).unwrap();
+
+    let host_seal_return = Func::wrap(&mut store, host_seal_return);
+    linker
+        .define("seal0", "seal_return", host_seal_return)
+        .unwrap();
+
+    linker
+        .define("env", "memory", memory)
+        .expect("We just created the Linker. It has no definitions with this name; qed");
+
+    let instance = linker
+        .instantiate(&mut store, &contract.module)
+        .map_err(|e| {
+            eprintln!("Error: {}", e);
+            Box::new(e) as Box<dyn std::error::Error>
+        })
+        .and_then(|instance| {
+            println!("Instance created!");
+            instance.start(&mut store).map_err(|e| {
+                eprintln!("Error starting instance: {}", e);
+                Box::new(e) as Box<dyn std::error::Error>
+            })
+        })?;
+
+    let deploy = instance
+        .get_typed_func::<(), ()>(&store, "deploy")
+        .map_err(|e| {
+            eprintln!("Error getting typed function 'deploy': {}", e);
+            Box::new(e) as Box<dyn std::error::Error>
+        })?;
 
     // And finally we can call the wasm!
-    hello.call(&mut store, ())?;
+    let result = deploy.call(&mut store, ());
+    println!("Result: {:?}", result);
+    let return_data = store.data().return_data.as_ref().unwrap();
+    println!("Return data: {:?}", return_data);
     Ok(())
 }
 
