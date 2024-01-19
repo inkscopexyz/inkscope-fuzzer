5f14238100ddaf51442cfca81960a1ddeedce065
0xLucca <0xlucca.dev@gmail.com>
2024-01-19 16:09:13 -0300

    Move cli to its own module

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,18 @@
+mod cli;
 mod ext_env;
+
 use clap::Parser;
 use ext_env::*;
-use std::{
-    collections::HashMap,
-    error::Error,
-    path::PathBuf,
-};
+use std::collections::HashMap;
 use wasmi::{
     core::Trap,
     *,
 };
-extern crate wabt;
 
-use wabt::wasm2wat;
-
-/// Simple cli to read files
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// The wat file of the contract to fuzz test
-    #[arg(long)]
-    wat: Option<PathBuf>,
-
-    /// The .contract file of the contract to fuzz test
-    #[arg(long)]
-    contract: Option<PathBuf>,
-
-    /// The .wasm file of the contract to fuzz test
-    #[arg(long)]
-    wasm: Option<PathBuf>,
-}
-
-fn get_wat_from_wat(path: PathBuf) -> Result<String, Box<dyn Error>> {
-    let wat = std::fs::read_to_string(path)?;
-    Ok(wat)
-}
-
-fn get_wat_from_wasm(path: PathBuf) -> Result<String, Box<dyn Error>> {
-    let wasm = std::fs::read(path)?;
-    let wat = wasm2wat(wasm)?;
-    Ok(wat)
-}
-
-fn get_wat_from_contract(path: PathBuf) -> Result<String, Box<dyn Error>> {
-    let contract = std::fs::read_to_string(path)?;
-    let contract: serde_json::Value = serde_json::from_str(&contract)?;
-    let wat = contract["wasm"]["wat"].as_str().unwrap();
-    Ok(wat.to_string())
-}
-
-fn get_wat(args: Args) -> Result<String, Box<dyn Error>> {
-    let args_count = args.wat.is_some() as usize
-        + args.contract.is_some() as usize
-        + args.wasm.is_some() as usize;
-    if args_count != 1 {
-        return Err("Please specify exactly one of --wat, --contract, or --wasm".into());
-    }
-    let wat = match args.wat {
-        Some(path) => get_wat_from_wat(path)?,
-        None => {
-            match args.contract {
-                Some(path) => get_wat_from_contract(path)?,
-                None => {
-                    match args.wasm {
-                        Some(path) => get_wat_from_wasm(path)?,
-                        None => {
-                            panic!("Please specify exactly one of --wat, --contract, or --wasm")
-                        }
-                    }
-                }
-            }
-        }
-    };
-    Ok(wat)
-}
+use cli::{
+    get_wat,
+    Args,
+};
 
 /// Set the value at the given key in the contract storage.
 fn host_set_storage(
