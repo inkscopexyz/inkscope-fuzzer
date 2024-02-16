05d2812ab89743e9a9146caf105ef8248bad8a2a
0xLucca <0xlucca.dev@gmail.com>
2024-02-16 18:21:45 -0300

    Testing drink

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,23 @@
-mod cli;
-mod utils;
-mod wasm;
-
-use clap::Parser;
-
-use cli::{
-    get_wat,
-    Args,
+use drink::{
+    runtime::MinimalRuntime,
+    session::{Session, NO_ARGS, NO_ENDOWMENT, NO_SALT},
+    ContractBundle,
 };
-
-use wasm::{
-    executor::*,
-    host_state::HostState,
-};
+use std::path::Path;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Parse command line arguments
-    let args = Args::parse();
+    let mut session = Session::<MinimalRuntime>::new()?;
+    let contract_path = Path::new("./flipper/target/ink/flipper.contract");
+    let contract = ContractBundle::load(contract_path).expect("Failed to load contract");
+    let init_value: bool = session
+        .deploy_bundle_and(contract, "new", &["true"], NO_SALT, NO_ENDOWMENT)?
+        .call_and("flip", NO_ARGS, NO_ENDOWMENT)?
+        .call_and("flip", NO_ARGS, NO_ENDOWMENT)?
+        .call_and("flip", NO_ARGS, NO_ENDOWMENT)?
+        .call_and("get", NO_ARGS, NO_ENDOWMENT)?
+        .record()
+        .last_call_return_decoded()?
+        .expect("Call was successful");
 
-    // Get the wasm from the command line arguments
-    let wat = get_wat(args)?;
-    let wasm = wat::parse_str(wat)?;
-
-    // Create a host state
-    let host_state = HostState::builder()
-        .input_buffer(vec![0xed, 0x4b, 0x9d, 0x1b])
-        .build();
-
-    // Create an executor
-    let executor = Executor::new(wasm, host_state)?;
-    let instance = executor.instance;
-    let mut store = executor.store;
-
-    let deploy = instance
-        .get_typed_func::<(), ()>(&store, "deploy")
-        .map_err(|e| {
-            eprintln!("Error getting typed function 'deploy': {}", e);
-            Box::new(e) as Box<dyn std::error::Error>
-        })?;
-
-    // And finally we can call the wasm!
-    let result = deploy.call(&mut store, ());
-    println!("Result: {:?}", result);
-    let return_data = store.data().return_data.as_ref().unwrap();
-    println!("Return data: {:?}", return_data);
     Ok(())
 }
-
-#[cfg(test)]
-#[path = "./tests/test.rs"]
-mod tests;
