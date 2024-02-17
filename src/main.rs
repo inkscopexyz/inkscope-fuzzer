36026f0cf212fec0d939e072f36b5719fb8085eb
0xLucca <0xlucca.dev@gmail.com>
2024-02-17 12:16:55 -0300

    One at a time actions

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut session = Session::<MinimalRuntime>::new()?;
     let contract_path = Path::new("./flipper/target/ink/flipper.contract");
     let contract = ContractBundle::load(contract_path).expect("Failed to load contract");
-    let init_value: bool = session
-        .deploy_bundle_and(contract, "new", &["true"], NO_SALT, NO_ENDOWMENT)?
-        .call_and("flip", NO_ARGS, NO_ENDOWMENT)?
-        .call_and("flip", NO_ARGS, NO_ENDOWMENT)?
-        .call_and("flip", NO_ARGS, NO_ENDOWMENT)?
-        .call_and("get", NO_ARGS, NO_ENDOWMENT)?
-        .record()
-        .last_call_return_decoded()?
-        .expect("Call was successful");
+
+    session.deploy_bundle(contract, "new", &["true"], NO_SALT, NO_ENDOWMENT)?;
+
+    let init_value: bool = session.call("get", NO_ARGS, NO_ENDOWMENT)??;
+    println!("Initial value: {}", init_value);
+
+    session.call("flip", NO_ARGS, NO_ENDOWMENT)??;
+
+    let value: bool = session.call("get", NO_ARGS, NO_ENDOWMENT)??;
+    println!("Value after flip: {}", value);
 
     Ok(())
 }
