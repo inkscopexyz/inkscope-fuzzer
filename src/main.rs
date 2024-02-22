f628097ff714013fbfbda56b4f24f3096c755cf3
0xLucca <0xlucca.dev@gmail.com>
2024-02-22 14:47:31 -0300

    Load contract from each thread

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,13 @@ use std::path::Path;
 use std::thread;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Load contract from file
-    let contract_path = Path::new("./flipper/target/ink/flipper.contract");
-    let contract = ContractBundle::load(contract_path).expect("Failed to load contract");
-
     // Get the number of available logical CPU cores
     let num_cpus = rayon::current_num_threads();
     println!("Number of CPU cores: {}", num_cpus);
 
     // Execute the main logic in parallel using Rayon
     (0..num_cpus).into_par_iter().for_each(|_| {
-        if let Err(err) = execute_main_logic(contract.clone()) {
+        if let Err(err) = execute_main_logic() {
             eprintln!("Error: {:?}", err);
         }
         println!("Thread {:?} finished", thread::current().id());
@@ -30,12 +26,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // }
     Ok(())
 }
-fn execute_main_logic(
-    contract: ContractBundle,
-) -> Result<(), Box<dyn std::error::Error>> {
+fn execute_main_logic() -> Result<(), Box<dyn std::error::Error>> {
     let mut session = Session::<MinimalRuntime>::new()?;
 
-    session.deploy_bundle(contract, "new", &["true"], NO_SALT, NO_ENDOWMENT)?;
+    // Load contract from file
+    let contract_path = Path::new("./flipper/target/ink/flipper.contract");
+    let contract = ContractBundle::load(contract_path).expect("Failed to load contract");
+
+    session.deploy_bundle(contract.clone(), "new", &["true"], NO_SALT, NO_ENDOWMENT)?;
 
     let init_value: bool = session.call("get", NO_ARGS, NO_ENDOWMENT)??;
     println!("Initial value: {}", init_value);
