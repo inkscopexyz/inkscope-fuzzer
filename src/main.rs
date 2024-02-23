f76ffab93fb112a6ffb63b3baaee7e156b658ad2
0xLucca <95830307+0xLucca@users.noreply.github.com>
2024-02-22 21:20:20 -0300

    Add rayon for multithreading support (#10)
    * Add Rayon
    
    * Use modified drink version
    
    * Load contract from each thread

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,37 @@ use drink::{
     session::{Session, NO_ARGS, NO_ENDOWMENT, NO_SALT},
     ContractBundle,
 };
+use rayon::prelude::*;
 use std::path::Path;
+use std::thread;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Get the number of available logical CPU cores
+    let num_cpus = rayon::current_num_threads();
+    println!("Number of CPU cores: {}", num_cpus);
+
+    // Execute the main logic in parallel using Rayon
+    (0..num_cpus).into_par_iter().for_each(|_| {
+        if let Err(err) = execute_main_logic() {
+            eprintln!("Error: {:?}", err);
+        }
+        println!("Thread {:?} finished", thread::current().id());
+    });
+
+    // let record = session.record().call_results();
+    // for result in record {
+    //     println!("{:?}\n", result);
+    // }
+    Ok(())
+}
+fn execute_main_logic() -> Result<(), Box<dyn std::error::Error>> {
     let mut session = Session::<MinimalRuntime>::new()?;
+
+    // Load contract from file
     let contract_path = Path::new("./flipper/target/ink/flipper.contract");
     let contract = ContractBundle::load(contract_path).expect("Failed to load contract");
 
-    session.deploy_bundle(contract, "new", &["true"], NO_SALT, NO_ENDOWMENT)?;
+    session.deploy_bundle(contract.clone(), "new", &["true"], NO_SALT, NO_ENDOWMENT)?;
 
     let init_value: bool = session.call("get", NO_ARGS, NO_ENDOWMENT)??;
     println!("Initial value: {}", init_value);
@@ -19,6 +42,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let value: bool = session.call("get", NO_ARGS, NO_ENDOWMENT)??;
     println!("Value after flip: {}", value);
+
+    // let record = session.record().call_results();
+    // for result in record {
+    //     println!("{:?}\n", result);
+    // }
 
     Ok(())
 }
