5b432bf2a56cabc3e10ffc39a5389cbb1f836bb4
Felipe Manzano <felipe.andres.manzano@gmail.com>
2024-03-05 21:29:06 -0300

    Working minimal example with output prettyfier

--- src/tests/primitive_generator.rs
+++ src/tests/primitive_generator.rs
@@ -8,14 +8,27 @@ pub mod primitive_generator_tests {
     use crate::RuntimeFuzzer;
     use std::path::PathBuf;
 
+    struct PTest{
+        path: PathBuf, 
+        max_rounds: usize,
+    }
+
     #[test]
     fn bool_message() {
-        // Initialize the fuzzer and the runtime
+        // TODO make ia fixture
+        // let all_tests = vec![ 
+        //     PTest{ path: "./test-contracts/primitive_generator_tester/target/ink/primitive_generator_tester.contract",
+        //            max_rounds: 100
+        //         },
+        // ];
+
         // Initialize the fuzzer and the runtime
         env_logger::init();
 
+        let path = "./test-contracts/primitive_generator_tester/target/ink/primitive_generator_tester.contract";
+        
         let mut fuzzer: RuntimeFuzzer = RuntimeFuzzer::new(PathBuf::from(
-            "./test-contracts/primitive_generator_tester/target/ink/primitive_generator_tester.contract",
+            path,
         ));
 
         let r = fuzzer.run();
