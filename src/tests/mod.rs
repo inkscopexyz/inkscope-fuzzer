272c0315b3ae2ce6eb9e4d0cde36a230f13f7729
0xLucca <0xlucca.dev@gmail.com>
2024-04-04 23:42:56 -0300

    Add tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,22 +1,23 @@
 #[cfg(test)]
-pub mod ityfuzz {
+pub mod testing {
 
     use crate::{
         config::Config,
-        constants::Constants,
         engine::Engine,
     };
-    use std::{
-        default,
-        path::PathBuf,
-    };
+    use std::path::PathBuf;
 
-    fn test_contract(contract_path: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+    fn test_contract(
+        contract_path: PathBuf,
+        should_find_broken_properties: bool,
+        max_rounds: Option<usize>,
+        max_number_of_transactions: Option<usize>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         // Set up the fuzzer configuration
         let config = Config {
             fail_fast: true,
-            max_rounds: 1000,
-            max_number_of_transactions: 50,
+            max_rounds: max_rounds.unwrap_or(10),
+            max_number_of_transactions: max_number_of_transactions.unwrap_or(50),
             ..Default::default()
         };
 
@@ -24,16 +25,39 @@ pub mod ityfuzz {
         let campaign_result = engine.run_campaign()?;
         engine.print_campaign_result(&campaign_result);
 
-        // Check that the campaign result found at least one failing trace
-        assert!(!campaign_result.failed_traces.is_empty());
+        // Check that the campaign result is as expected
+        if should_find_broken_properties {
+            assert!(!campaign_result.failed_traces.is_empty());
+        } else {
+            assert!(campaign_result.failed_traces.is_empty());
+        }
 
         Ok(())
     }
 
     #[test]
     fn fuzz_ityfuzz() -> Result<(), Box<dyn std::error::Error>> {
+        test_contract(
+            PathBuf::from("./test-contracts/ityfuzz/target/ink/ityfuzz.contract"),
+            true,
+            Some(1000),
+            None,
+        )
+    }
+
+    #[test]
+    fn fuzz_integer_overflow_or_underflow_1_vulnerable(
+    ) -> Result<(), Box<dyn std::error::Error>> {
         test_contract(PathBuf::from(
-            "./test-contracts/ityfuzz/target/ink/ityfuzz.contract",
-        ))
+            "./test-contracts/coinfabrik-test-contracts/integer-overflow-or-underflow-1/vulnerable-example/target/ink/integer_overflow_or_underflow.contract", 
+        ),true,Some(100),None)
+    }
+
+    #[test]
+    fn fuzz_integer_overflow_or_underflow_1_remediated(
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        test_contract(PathBuf::from(
+            "./test-contracts/coinfabrik-test-contracts/integer-overflow-or-underflow-1/remediated-example/target/ink/integer_overflow_or_underflow.contract", 
+        ),false,Some(10),Some(10))
     }
 }
---
+++ test-contracts/coinfabrik-test-contracts/integer-overflow-or-underflow-1/remediated-example/lib.rs
@@ -0,0 +1,84 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[ink::contract]
+mod integer_overflow_underflow {
+
+    #[ink(storage)]
+    pub struct IntegerOverflowUnderflow {
+        /// Stored value.
+        value: u8,
+    }
+
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        /// An overflow was produced while adding
+        OverflowError,
+        /// An underflow was produced while substracting
+        UnderflowError,
+    }
+
+    impl IntegerOverflowUnderflow {
+        /// Creates a new instance of the contract.
+        #[ink(constructor)]
+        pub fn new(value: u8) -> Self {
+            Self { value }
+        }
+
+        /// Increments the stored value by the given amount.
+        #[ink(message)]
+        pub fn add(&mut self, value: u8) -> Result<(), Error> {
+            match self.value.checked_add(value) {
+                Some(v) => self.value = v,
+                None => return Err(Error::OverflowError),
+            };
+            Ok(())
+        }
+
+        /// Decrements the stored value by the given amount.
+        #[ink(message)]
+        pub fn sub(&mut self, value: u8) -> Result<(), Error> {
+            match self.value.checked_sub(value) {
+                Some(v) => self.value = v,
+                None => return Err(Error::UnderflowError),
+            };
+            Ok(())
+        }
+
+        /// Returns the stored value.
+        #[ink(message)]
+        pub fn get(&self) -> u8 {
+            self.value
+        }
+    }
+
+    #[cfg(feature = "fuzz-testing")]
+    #[ink(impl)]
+    impl IntegerOverflowUnderflow {
+        #[cfg(feature = "fuzz-testing")]
+        #[ink(message)]
+        pub fn inkscope_overflow(&mut self, value: u8) -> bool {
+            // Save the initial value
+            let init_value = self.value;
+
+            // Add the received value
+            self.add(value);
+
+            // Return false if it overflowed
+            init_value <= self.value
+        }
+
+        #[cfg(feature = "fuzz-testing")]
+        #[ink(message)]
+        pub fn inkscope_underflow(&mut self, value: u8) -> bool {
+            // Save the initial value
+            let init_value = self.value;
+
+            // Subtract the received value
+            self.sub(value);
+
+            // Return false if it underflowed
+            init_value >= self.value
+        }
+    }
+}
