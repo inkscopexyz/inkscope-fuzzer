9ccb67de13cd633decb8152a5bf6d6b0609be96e
0xLucca <0xlucca.dev@gmail.com>
2024-04-11 00:00:40 -0300

    Improve build.py and add clean.py

--- test-contracts/build.py
+++ test-contracts/build.py
@@ -7,21 +7,34 @@ def build_contracts(current_dir):
     if os.path.isfile(os.path.join(current_dir, "Cargo.toml")):
         print(f"Building contract in directory: {current_dir}")
 
-        # Execute the build command
-        subprocess.run(["cargo", "contract", "build", "--features", "fuzz-testing"], cwd=current_dir)
+        # Check if the targer dir exists
+        target_dir = os.path.join(current_dir, "target")
 
-        # Find the .contract file and modify the version field
-        contract_file = next((os.path.join(current_dir, "target", "ink", f) for f in os.listdir(os.path.join(current_dir, "target", "ink")) if f.endswith(".contract")), None)
-        if contract_file:
-            print(f"Parsing and modifying .contract file: {contract_file}")
-            with open(contract_file, "r") as f:
-                contract_data = json.load(f)
-            contract_data["version"] = int(contract_data["version"])
-            with open(f"{contract_file}.tmp", "w") as f:
-                json.dump(contract_data, f)
-            os.rename(f"{contract_file}.tmp", contract_file)
+        # If it exists and the .contract file also exists, skip the build
+        if os.path.exists(target_dir):
+            contract_file = next((os.path.join(current_dir, "target", "ink", f) for f in os.listdir(os.path.join(current_dir, "target", "ink")) if f.endswith(".contract")), None)
         else:
-            print(f".contract file not found in directory: {current_dir}")
+            contract_file = None
+            
+        if not contract_file:
+
+            # Execute the build command
+            subprocess.run(["cargo", "contract", "build", "--features", "fuzz-testing"], cwd=current_dir)
+
+            # Find the .contract file and modify the version field
+            contract_file = next((os.path.join(current_dir, "target", "ink", f) for f in os.listdir(os.path.join(current_dir, "target", "ink")) if f.endswith(".contract")), None)
+            if contract_file:
+                print(f"Parsing and modifying .contract file: {contract_file}")
+                with open(contract_file, "r") as f:
+                    contract_data = json.load(f)
+                contract_data["version"] = int(contract_data["version"])
+                with open(f"{contract_file}.tmp", "w") as f:
+                    json.dump(contract_data, f)
+                os.rename(f"{contract_file}.tmp", contract_file)
+            else:
+                print(f".contract file not found in directory: {current_dir}")
+        else:
+            print(f".contract file already exists in {current_dir}, skipping build.")
 
     # Recursively search in all subdirectories
     for subdir in [os.path.join(current_dir, d) for d in os.listdir(current_dir) if os.path.isdir(os.path.join(current_dir, d))]:
